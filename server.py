3ee662e8d73c4aef5380165e767f623a06515f47
Bashar <bhamdanieh@gmail.com>
2024-04-15 18:50:45 +0300

    Adding Projects Admin Feature/Image Uploading Fix

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ import os
 
 # Flask WTForms imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField, PasswordField, TextAreaField
+from wtforms import StringField, SubmitField, EmailField, PasswordField, TextAreaField, FileField
 from wtforms.validators import DataRequired, EqualTo, Email, Length
 from flask_ckeditor import CKEditor, CKEditorField
 
@@ -26,6 +26,7 @@ from dateutil import parser as date_parser
 # SMTP imports
 import smtplib
 
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -51,6 +52,13 @@ class Article(db.Model):
     body: Mapped[str] = mapped_column(String, nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
+class Project(db.Model):
+    __tablename__ = "projects"
+    id: Mapped[int] =mapped_column(Integer, primary_key=True)
+    excerpt: Mapped[str] = mapped_column(String, nullable=False)
+    img_url: Mapped[str] = mapped_column(String, nullable=False)
+    web_url: Mapped[str] = mapped_column(String, nullable=False)
+
 with app.app_context():
     db.create_all()
     # article3 = Article(title='Embrace the Fun: Exploring the Joy of Learning CSS and HTML',
@@ -60,6 +68,7 @@ with app.app_context():
     #                    img_url= 'https://img.freepik.com/free-vector/programmers-concept-with-flat-design_23-2147852753.jpg?t=st=1712997608~exp=1713001208~hmac=0c830db79897932dd53924a29dd4ace0208deb0641d99c5824e5de67ffa9959e&w=826')
     # db.session.add(article3)
     # db.session.commit()
+
 
 
 ### Forms
@@ -76,10 +85,19 @@ class ContactForm(FlaskForm):
 class CreatePostForm(FlaskForm):
     title    = StringField("Article Title", validators=[DataRequired()])
     subtitle = StringField("Article Subtitle", validators=[DataRequired()])
-    img_url    = StringField("Article IMG URL", validators=[DataRequired()])
+    img_url    = FileField("Article Featured Image", validators=[DataRequired()])
+    img_url_edit    = FileField("Article Featured Image")
     body     = CKEditorField("Body", validators=[DataRequired()])
     submit   = SubmitField("Submit")
     submit_edit = SubmitField("Submit")
+
+
+class AddProjectForm(FlaskForm):
+    excerpt     = StringField("Project Except/Description", validators=[DataRequired()])
+    image       = FileField("Project Featured Image", validators=[DataRequired()])
+    web_url     = StringField("Project URL", validators=[DataRequired()])
+    submit      = SubmitField("Submit")
+
 
 
 # Functions
@@ -102,7 +120,8 @@ def send_mail(name, email, message):
 @app.route("/")
 def homepage():
     articles = Article.query.all()
-    return render_template("index.html", articles = articles)
+    projects = Project.query.all()
+    return render_template("index.html", articles = articles, projects=projects)
 
 
 @app.route('/articles')
@@ -137,13 +156,20 @@ def add_article_page():
 
     if add_article_form.validate_on_submit():
         if request.method == 'POST':
+            image_file = request.files['img_url']
+            filename = secure_filename(image_file.filename)
+            path = f'static/images/uploads/{filename}'
+            image_file.save(path)
+
+            
             title = request.form['title']
             subtitle = request.form['subtitle']
-            img_url = request.form['img_url']
             body = add_article_form.body.data
-            new_article = Article(title=title, subtitle=subtitle, img_url=img_url, body=body, date=today_date)
+        
+            new_article = Article(title=title, subtitle=subtitle, img_url=path, body=body, date=today_date)
             db.session.add(new_article)
             db.session.commit()
+        
             flash("Article Added Successfully", "post_added")
             return redirect(url_for('add_article_page'))
 
@@ -159,13 +185,23 @@ def edit_article_page():
 @app.route('/edit-article-form/<article_id>', methods=['GET', 'POST'])
 def edit_article_form_page(article_id):
     article = Article.query.filter(Article.id == article_id).one()
-    edit_article_form = CreatePostForm(title=article.title, subtitle=article.subtitle, img_url=article.img_url, body=article.body) 
+    edit_article_form = CreatePostForm(title=article.title, img_url=article.img_url,subtitle=article.subtitle, body=article.body) 
 
     if edit_article_form.validate_on_submit():
         if request.method == 'POST':
+
+            image_file = request.files['img_url_edit']
+            if image_file:
+                filename = secure_filename(image_file.filename)
+                path = f'static/images/uploads/{filename}'
+                image_file.save(path)
+                article.img_url = path
+            else:
+                article.img_url = article.img_url
+
+
             article.title = request.form['title']
             article.subtitle = request.form['subtitle']
-            article.img_url = request.form['img_url']
             article.body = request.form['body']
             article.date = article.date
             db.session.commit()
@@ -188,8 +224,44 @@ def delete_article(article_id):
     return jsonify(error= 'Article Not Found'), 404
         
 
+@app.route('/add-project', methods=['GET', 'POST'])
+def add_project_page():
+    form = AddProjectForm()
 
+    if form.validate_on_submit():
+        if request.method == "POST":
+            image_file = request.files['image']
+            filename = secure_filename(image_file.filename)
+            path = f'static/images/uploads/{filename}'
+            image_file.save(path)
+            print(f"File saved as {filename}")
+            
+            
+            new_project = Project(excerpt=form.excerpt.data, img_url=path, web_url=form.web_url.data)
+            db.session.add(new_project)
+            db.session.commit()
 
+            flash("Project Added Successfully", "project_added")
+            return redirect(url_for('add_project_page'))
+        
+    return render_template('add_project.html', form=form)
+
+@app.route('/edit-project')
+def edit_project_page():
+    projects = Project.query.all()
+    return render_template('edit_project.html', projects=projects)
+
+@app.route('/delete-project/<project_id>', methods=['GET', 'POST'])
+def delete_project(project_id):
+    project = Project.query.filter(Project.id == project_id).one()
+
+    if project:
+        db.session.delete(project)
+        db.session.commit()
+        flash("Project Deleted Successfully", "project_deleted")
+        return redirect(url_for('edit_project_page'))
+    
+    return jsonify(error= 'Project Not Found'), 404
 
 
 if __name__ == "__main__":
